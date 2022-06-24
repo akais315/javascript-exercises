c77705bdb7862e4c89ba83651a6555629c736d69
akais315 <akais315@gmail.com>
2022-06-24 10:22:32 +0200

    Complete Ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(start < 0 || end < 0){ return "ERROR"}
+    if(Number(start) !== start || Number(end) !== end){return "ERROR"}
+    let answer = 0
+    if(start <= end){
+        for(let i = start; i <= end; i++){
+            answer += i;
+        }
+    } else {
+        for(let i = start; i >= end; i--){
+            answer += i;
+        }
+    }
+    return answer
 };
 
 // Do not edit below this line
