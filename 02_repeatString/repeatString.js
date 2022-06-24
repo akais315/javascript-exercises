e0b88936f0653089938920b3e21b50f642baab59
akais315 <akais315@gmail.com>
2022-06-24 09:04:58 +0200

    Complete Ex 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = ""
+    if (num < 0) return "ERROR";
+    for(let i = num; i > 0; i--){
+        text += string;
+    }
+    return text
 };
 
 // Do not edit below this line
