fe6dbf265094fca26c14c81155b0eba13165198a
akais315 <akais315@gmail.com>
2022-06-24 10:59:22 +0200

    Complete Ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(...args) {
+	let result = 0;
+  for(const arg of args){
+    result += arg;
+  }
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(...args) {
+	return args[0]-args[1]
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for(const value of array){result += value}
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for(const value of array){ result *= value}
+  return result
 };
 
-const power = function() {
-	
+const power = function(num, exp) {
+	return num**exp
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for(let i = num; i > 0 ; i--){
+    result *= i;
+  }
+  return result
 };
 
 // Do not edit below this line
