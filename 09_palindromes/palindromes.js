48c643a15f26abec8ed3380c94d776967a273453
akais315 <akais315@gmail.com>
2022-06-24 11:20:26 +0200

    Complet Ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~/ ]/g;
+    string = string.replace(regex,'').toLowerCase();
+    return string.split('').reverse().join('') === string
 };
 
 // Do not edit below this line
