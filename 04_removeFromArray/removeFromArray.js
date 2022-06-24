2357d61ac88dce629c4217575921d6d7f302f164
akais315 <akais315@gmail.com>
2022-06-24 10:03:50 +0200

    Complete Ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let index = -1
+    for(const arg of args){
+        index = array.indexOf(arg);
+        if(index >= 0){
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
