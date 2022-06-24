dc6dc144eed3f19727a6298cbd5ff4fdf49230cc
akais315 <akais315@gmail.com>
2022-06-24 11:51:44 +0200

    Complete Ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let result = [0, 0];
+    for(const person of array){
+        let age = findAge(person.yearOfBirth, person.yearOfDeath);
+        if(age > result[0]) result = [age, person];
+    }
 
+    return result[1];
+
+    function findAge(yearOfBirth, yearOfDeath = 2022){
+        return yearOfDeath-yearOfBirth;
+    }
 };
 
 // Do not edit below this line
