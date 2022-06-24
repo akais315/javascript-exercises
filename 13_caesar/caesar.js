f90469637bf51019527ea385c1ecb9d35e9c9367
akais315 <akais315@gmail.com>
2022-06-24 16:30:09 +0200

    Complete Ex 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,18 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    code = string.split('').map(char => char.charCodeAt());
+    shiftedCode = code.map(char => shiftChar(char, shift)).map(char => String.fromCharCode(char)).join("");
+    return shiftedCode;
 
+    function shiftChar(char, shift){
+        const type = char => char < 97 ? 65 : 97;
+        if((char <= 90 && char >= 65) || (char >= 97 && char <= 122 )){
+            let charType = type(char);
+            char = char + shift;
+            while(char < charType) char += 26;
+            while(char >= charType+25) char -= 26;
+        }
+        return char;
+    }
 };
 
 // Do not edit below this line
